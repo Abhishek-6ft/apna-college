7164c37876f733b00df7eec65a887e0ad388e5dc
Abhishek <Abhishek@isFuture.com>
2024-08-17 21:21:28 +0530

    Arrays file was updated

--- day08_Array/Array.java
+++ day08_Array/Array.java
@@ -1,36 +1,36 @@
 package day08_Array;
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
 public class Array {
 
     public static void main(String args[]){
         int marks[]= new int[49];
         System.out.println("the length of array = " + marks.length);
-        // @SuppressWarnings("resource")
-        // Scanner sc = new Scanner(System.in);
-        // int phys;
-        // phys = sc.nextInt();
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        int phys;
+        phys = sc.nextInt();
 
-        // marks[0] = sc.nextInt(); // phys
-        // marks[1] = sc.nextInt(); // chem
-        // marks[2] = sc.nextInt(); //math
+        marks[0] = sc.nextInt(); // phys
+        marks[1] = sc.nextInt(); // chem
+        marks[2] = sc.nextInt(); //math
 
-        // System.out.println("physics: " + marks[0] + ", Chemsitry: " + marks[1] + ", Math: " + marks[2]+";");
+        System.out.println("physics: " + marks[0] + ", Chemsitry: " + marks[1] + ", Math: " + marks[2]+";");
 
-        // int percentage = (marks[0] + marks[1] + marks[2]) / 3;
-        // System.out.println("Percentage = " + percentage + "%");
-
-
-
-        // marks[2] = marks[2] + 2;
-        // System.out.println("Math: " + marks[2]);
+        int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+        System.out.println("Percentage = " + percentage + "%");
 
 
-        // int number[] = {1,2,3};
 
-        // int moreNumbers[] = {4,5,6};
+        marks[2] = marks[2] + 2;
+        System.out.println("Math: " + marks[2]);
 
-        // String fruits[] = {"apple", "mango", "orange"};
+
+        int number[] = {1,2,3};
+
+        int moreNumbers[] = {4,5,6};
+
+        String fruits[] = {"apple", "mango", "orange"};
     }
 }
