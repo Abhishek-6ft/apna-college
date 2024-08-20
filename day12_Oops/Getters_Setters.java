0331c32026574f9ef5450f7e6b83c619da3898c1
Abhishek <Abhishek@isFuture.com>
2024-08-20 07:19:38 +0530

    file was updated

--- day12_Oops/Getters_Setters.java
+++ day12_Oops/Getters_Setters.java
@@ -4,3 +4,24 @@ public class Getters_Setters {
     
 }
 
+class Pen {
+    private String color;
+    private int tip;
+    // getters function
+    String getColor(){
+        return this.color;
+    }
+    int getTip(){
+        return this.tip;
+    }
+
+    // Setters functions
+    void setColor(String newColor){
+        this.color = newColor;
+    }
+    
+    void setTip(int newTip){
+        this.tip = newTip;
+    }
+}
+
