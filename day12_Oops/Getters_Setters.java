0241cb268f03b07ff32f21616fba19c34a9a35fc
Abhishek <hii.abhishek.cloud@gmail.com>
2024-08-20 07:06:07 +0530

    Update Getters_Setters.java

--- day12_Oops/Getters_Setters.java
+++ day12_Oops/Getters_Setters.java
@@ -1,5 +1,8 @@
 package day12_Oops;
-
+// Getters & Setters 
+// Get : to return the value
+// Set : to modify the value 
+// this : this keyword is used to refer to the current object
 public class Getters_Setters {
     
 }
