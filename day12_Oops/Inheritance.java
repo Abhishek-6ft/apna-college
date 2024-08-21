d88d56134c1da7fca8c099065be312ba002dd1d6
Abhishek <hii.abhishek.cloud@gmail.com>
2024-08-21 07:59:15 +0530

    Update Inheritance.java

--- day12_Oops/Inheritance.java
+++ day12_Oops/Inheritance.java
@@ -5,6 +5,11 @@ package day12_Oops;
 
     Inheritance is when properties & method of base class are passed on to ta derived class
 
+
+    Types of Iheritance
+
+    1. Single 
+
 */
 public class Inheritance {
     public static void main(String[] args) {
