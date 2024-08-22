305ce4db5c3ea3d4b25b9d2e44ef42637bb1a68f
Abhishek <hii.abhishek.cloud@gmail.com>
2024-08-22 08:33:23 +0530

    Update Inheritance.java

--- day12_Oops/Inheritance.java
+++ day12_Oops/Inheritance.java
@@ -1,15 +1,17 @@
 package day12_Oops;
 /* 
-
     Inheritance
-
     Inheritance is when properties & method of base class are passed on to ta derived class
-
-
+    
     Types of Iheritance
 
-    1. Single 
+    1. Single Level Inheritance
+    Base Class ---> Derived Class
+    Example --> below (Animal)
 
+    2. Multi Level Inheritance
+    Base Class --> Derived Class --> Derived Class
+    Example : Animal(func eat,breathe prop. color) --> Mammals(prop. legs) --> Dog(breed)
 */
 public class Inheritance {
     public static void main(String[] args) {
