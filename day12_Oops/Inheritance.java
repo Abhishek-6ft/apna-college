21e921fd5014fa31ec20296d0c33fab01da0dd37
Abhishek <Abhishek@isFuture.com>
2024-08-22 08:34:07 +0530

    file was successfully updated

--- day12_Oops/Inheritance.java
+++ day12_Oops/Inheritance.java
@@ -9,5 +9,31 @@ package day12_Oops;
 public class Inheritance {
     public static void main(String[] args) {
         
+        Fish shark = new Fish();
+        shark.eat();
+        // shark.breathe();
+    }
+}
+
+// Base class
+
+class Animal {
+    String color;
+
+    void eat(){
+        System.out.println("eats");
+    }
+
+    void breathe(){
+        System.out.println("breathe");
+    }
+}
+
+// Drive Class 
+class Fish extends Animal{
+    int fins;
+
+    void swim(){
+        System.out.println("Swim in water");
     }
 }
