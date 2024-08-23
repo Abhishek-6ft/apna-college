ef8dc436c8b435d956d1991d613695e98be49c5a
Abhishek <Abhishek@isFuture.com>
2024-08-23 08:37:40 +0530

    file change success

--- day12_Oops/Polymorphism.java
+++ day12_Oops/Polymorphism.java
@@ -12,9 +12,9 @@ package day12_Oops;
 public class Polymorphism {
     public static void main(String args[]){
         Calculator calc = new Calculator();
-        system.out.println(calc.sum(1, 2));
-        system.out.println(calc.sum(1.5, 2.5));
-        system.out.println(calc.sum(1, 2, 3));
+        System.out.println(calc.sum(1, 2));
+        System.out.println(calc.sum((float)1.5, (float)2.5));
+        System.out.println(calc.sum(1, 2, 3));
     }
 }
 
@@ -22,7 +22,7 @@ class Calculator{
     int sum(int a, int b){
         return a + b;
     }
-    float sum(int a, int b){
+    float sum(float a, float b){
         return a +b;
     }
     int sum(int a, int b, int c){
