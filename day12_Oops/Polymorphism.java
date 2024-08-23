85d58a8d7726f5c6f53eb5c8a3a183420a5669b3
Abhishek <hii.abhishek.cloud@gmail.com>
2024-08-23 08:47:46 +0530

    Update Polymorphism.java

--- day12_Oops/Polymorphism.java
+++ day12_Oops/Polymorphism.java
@@ -7,14 +7,21 @@ package day12_Oops;
         // . Mehtod Overriding
 
 // Method Overloading
-// - Multiple functions with the same name but different parameters
+// - Multiple functions with the same name but different parameters.
+
 
 public class Polymorphism {
-    public static void main(String args[]){
+    public static void main(String args
+                            
+        // Example of method overloading
         Calculator calc = new Calculator();
         System.out.println(calc.sum(1, 2));
         System.out.println(calc.sum((float)1.5, (float)2.5));
         System.out.println(calc.sum(1, 2, 3));
+
+        // call method overriding example
+        Deer d = new Deer();
+        d.eat();
     }
 }
 
@@ -28,4 +35,18 @@ class Calculator{
     int sum(int a, int b, int c){
         return a + b +c;
     }
+}
+// Method Overriding
+// - Parent and child classes both contain the same function with a different definition.
+
+class Animal{
+        void eat(){
+          System.out.println("Eats Anything");                
+        }
+}
+
+class Deer extends Animal{
+        void eat(){
+          System.out.println("Eat Grass");                
+        }
 }
