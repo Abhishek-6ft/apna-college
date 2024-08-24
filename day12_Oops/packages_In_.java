9b95ec87060453896543c55cb01fbdd545db95bc
Abhishek <hii.abhishek.cloud@gmail.com>
2024-08-24 08:30:26 +0530

    Update packages_In_.java

--- day12_Oops/packages_In_.java
+++ day12_Oops/packages_In_.java
@@ -1,7 +1,13 @@
 package day12_Oops;
+import.java.util.*;
 
+// Java has two types of packages
+// 1. Inbuilt packages
+// 2. User Defined packages
 public class packages_In_ {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
     }
 }
