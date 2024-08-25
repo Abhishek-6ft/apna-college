58f887f749043001d97e981e6ff8496e0a75f354
Abhishek <Abhishek@isFuture.com>
2024-08-25 08:11:24 +0530

    first question was done

--- day12_Oops/Practice_Question.java
+++ day12_Oops/Practice_Question.java
@@ -2,6 +2,18 @@ package day12_Oops;
 
 public class Practice_Question {
     public static void main(String[] args) {
-        
+        // Question first find out the correct statement to assign name to object
+        Studentfit s = new Studentfit();
+        s.name = "abhishek";
+        // System.out.println(s.name);
+
+        // Question second which variable can the class person access in the following` code?
     }
+}
+
+//  Question 1
+
+class Studentfit {
+    String name;
+    int roll;
 }
