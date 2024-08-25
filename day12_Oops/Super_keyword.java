c135ad4fa1896a0ac971109026bd26c475dc521c
Abhishek <hii.abhishek.cloud@gmail.com>
2024-08-25 08:01:11 +0530

    Update Super_keyword.java

--- day12_Oops/Super_keyword.java
+++ day12_Oops/Super_keyword.java
@@ -1,7 +1,27 @@
 package day12_Oops;
+// Super keyword
+
+// Super keyword is used to refer immediate parent class object
+// -to access parent's properties
+// -to access parent's functions
+// -to access parent's constructor
 
 public class Super_keyword {
     public static void main(String[] args) {
+        HorseIndian broce = new HorseIndian();
         
+    }
+}
+
+class IndianAnimal{
+    IndianAnimal(){
+        System.out.println("animal constructor is called");
+    }
+}
+
+class HorseIndian extends IndianAnimal {
+    HorseIndian(){
+        super();
+        System.out.println("Horse constructor is called");   
     }
 }
