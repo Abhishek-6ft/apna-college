71d8e8ab05ba6bf424bc1b1490c2960340dc1e60
Abhishek <Abhishek@isFuture.com>
2024-08-25 06:42:13 +0530

    add interface example in practice file

--- day12_Oops/practice.java
+++ day12_Oops/practice.java
@@ -2,33 +2,57 @@ package day12_Oops;
 
 public class practice {
     public static void main(String[] args) {
-        StudentBioData infoStudentfirst = new StudentBioData();
-        infoStudentfirst.name = "Abhishek kumar";
-        infoStudentfirst.roll = 21;
-        infoStudentfirst.standard = "B.com pursuing from DU (School of open learning)";
-        infoStudentfirst.studentID = 223430009;
-        infoStudentfirst.DOB = "7/10/2104";
-        infoStudentfirst.currentYearPercentage = 73.25;
-        infoStudentfirst.previousYearPercentage = 71.50;
-        infoStudentfirst.marksCurrentYear = "69, 71, 70";
-        infoStudentfirst.marksPreviousYear = "66, 70, 72";
-        infoStudentfirst.studentFatherName = "Surender";
-        infoStudentfirst.studentMotherName = "simpy";
 
+        // interface Example
+
+
+        // StudentBioData infoStudentfirst = new StudentBioData();
+        // infoStudentfirst.name = "Abhishek kumar";
+        // infoStudentfirst.roll = 21;
+        // infoStudentfirst.standard = "B.com pursuing from DU (School of open learning)";
+        // infoStudentfirst.studentID = 223430009;
+        // infoStudentfirst.DOB = "7/10/2104";
+        // infoStudentfirst.currentYearPercentage = 73.25;
+        // infoStudentfirst.previousYearPercentage = 71.50;
+        // infoStudentfirst.marksCurrentYear = "69, 71, 70";
+        // infoStudentfirst.marksPreviousYear = "66, 70, 72";
+        // infoStudentfirst.studentFatherName = "Surender";
+        // infoStudentfirst.studentMotherName = "simpy";
+    
+        // System.out.println(infoStudentfirst.name);
+        // System.out.println(infoStudentfirst.DOB);
+    
+    
+    
+        // StudentBioData infoStudentSecond = new StudentBioData(infoStudentfirst);
+        // infoStudentfirst.DOB = "7/2/2004";
+        // infoStudentfirst.name = "sunny";
+    
+        
         // System.out.println(infoStudentfirst.name);
         // System.out.println(infoStudentfirst.DOB);
 
+        Bear b = new Bear();
+        b.vegetarian();
+        b.Non_vegetarian();
+    }    
+}
 
+interface Herbivorus{
+    void vegetarian();
+}
 
-        StudentBioData infoStudentSecond = new StudentBioData(infoStudentfirst);
-        infoStudentfirst.DOB = "7/2/2004";
-        infoStudentfirst.name = "sunny";
+interface Carnivorus{
+    void Non_vegetarian();
+}
 
-        
-        System.out.println(infoStudentfirst.name);
-        System.out.println(infoStudentfirst.DOB);
+class Bear implements Herbivorus, Carnivorus{
+    public void vegetarian() {
+        System.out.println("i like grass");
+    }
 
-
+    public void Non_vegetarian(){
+        System.out.println("i also like meat baby");
     }
 }
 
