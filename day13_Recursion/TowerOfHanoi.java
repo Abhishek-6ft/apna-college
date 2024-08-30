eb31b1ecdc5c3e1f22cf8aab0bb585e8308f694a
Abhishek <Abhishek@isFuture.com>
2024-08-30 08:35:19 +0530

    file updated

--- day13_Recursion/TowerOfHanoi.java
+++ day13_Recursion/TowerOfHanoi.java
@@ -1,7 +1,19 @@
 package day13_Recursion;
 
 public class TowerOfHanoi {
+    public static void hanoiTower(int n, String src, String helper, String destination){
+        // base case
+        if (n == 1) {
+            System.out.println("Transfer disk " + n + " from " + src + " to " +destination);
+            return;
+        }
+
+        hanoiTower(n-1, src, destination, helper);
+        System.out.println("Transfer disk " + n + " from " + src + " to " +destination);
+        hanoiTower(n-1, helper, src, destination);
+    }
     public static void main(String[] args) {
-        
+        int n = 2;
+        hanoiTower(n, "Source", "Helper", "Destination");
     }
 }
