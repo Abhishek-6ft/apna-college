ef345eb29883a027b189fa772aad4a8e084798df
Abhishek <Abhishek@isFuture.com>
2024-08-31 07:44:54 +0530

    comment added

--- day13_Recursion/TilingProblem.java
+++ day13_Recursion/TilingProblem.java
@@ -1,5 +1,10 @@
 package day13_Recursion;
-
+/*
+ * Tiling Problem
+ * Given a "2 * n" board and tiles of the size "2 * 1" count the number of ways to 
+ * tile the given board using the the 2 * 1 tiles.
+ * (A tile can either be placed horizontally or vertically)
+ */
 public class TilingProblem {
     public static 
     public static void main(String[] args) {
