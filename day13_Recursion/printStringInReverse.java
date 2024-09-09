e164d20abd85b3c48f444e301e917c310314ac1d
Abhishek <Abhishek@isFuture.com>
2024-09-09 07:31:19 +0530

    question updated

--- day13_Recursion/printStringInReverse.java
+++ day13_Recursion/printStringInReverse.java
@@ -3,7 +3,16 @@ package day13_Recursion;
 // "appnnacollege"
 // Google question
 public class printStringInReverse {
+    public static void printReverse(String str, int indx){
+        if (indx == 0) {
+            System.out.println(str.charAt(indx));
+            return;
+        }
+        System.out.print(str.charAt(indx));
+        printReverse(str, indx-1);
+    }
     public static void main(String[] args) {
-        
+        String str = "abcd";
+        printReverse(str, str.length()-1);
     }
 }
