5289a0a5d1c4094f9e1d969000a5335630a7c8b8
Abhishek <Abhishek@isFuture.com>
2024-09-10 07:45:16 +0530

    file updated

--- day13_Recursion/ProblemsOfRecursion.java
+++ day13_Recursion/ProblemsOfRecursion.java
@@ -1,8 +1,25 @@
 package day13_Recursion;
 
 public class ProblemsOfRecursion {
-    
+    // Question 1 Remove duplicates in a string
+    public static void removeDuplicates(String str, int index, StringBuilder newString, boolean map[]){
+        // base case
+        if(index == str.length()){
+            System.out.println(newString);
+            return;
+        }
+
+        // work
+        char currentChar = str.charAt(index);
+        if (map[currentChar-'a'] == true) {
+            removeDuplicates(str, index+1, newString, map);
+        } else{
+            map[currentChar-'a'] = true;
+            removeDuplicates(str, index+1, newString.append(currentChar), map);
+        }
+    }
     public static void main(String[] args) {
-        
+        String str = "appnacollege";
+        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
     }
 }
