e4f6b5a7b4bf899547ecda901c57de47956c6dd3
Abhishek <Abhishek@isFuture.com>
2024-09-11 08:48:27 +0530

    merge sort added

--- day14_Divide_Conquer/mergeSort.java
+++ day14_Divide_Conquer/mergeSort.java
@@ -2,56 +2,56 @@ package day14_Divide_Conquer;
 
 public class mergeSort {
 
-    // public static void printArr(int arr[]){
-    //     for(int i=0; i<arr.length; i++){
-    //         System.out.print(arr[i]+ " ");
-    //     }
-    //     System.out.println();
-    // }
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+    }
 
-    // public static void mergeSortt(int arr[], int startingIndex, int endingIndex){
-    //     if (startingIndex >= endingIndex) {
-    //         return;
-    //     }
+    public static void mergeSortt(int arr[], int startingIndex, int endingIndex){
+        if (startingIndex >= endingIndex) {
+            return;
+        }
 
-    //     // kaam
-    //     int mid = startingIndex + (endingIndex - startingIndex) / 2;
-    //     mergeSortt(arr, startingIndex, mid); // left part
-    //     mergeSortt(arr, mid+1, endingIndex); // right part
+        // kaam
+        int mid = startingIndex + (endingIndex - startingIndex) / 2;
+        mergeSortt(arr, startingIndex, mid); // left part
+        mergeSortt(arr, mid+1, endingIndex); // right part
 
-    //     merge(arr, startingIndex, mid, endingIndex);
-    // }
+        merge(arr, startingIndex, mid, endingIndex);
+    }
 
-    // public static void merge(int arr[], int startingIndex, int mid, int endingIndex){
-    //     int temp[] = new int[endingIndex - startingIndex +1];
-    //     int i = startingIndex; // iterator for left part
-    //     int j = mid+1; //iterator for right part
-    //     int k = 0;// iterator for temp array
+    public static void merge(int arr[], int startingIndex, int mid, int endingIndex){
+        int temp[] = new int[endingIndex - startingIndex +1];
+        int i = startingIndex; // iterator for left part
+        int j = mid+1; //iterator for right part
+        int k = 0;// iterator for temp array
         
-    //     while (i <= mid && j <= endingIndex) {
-    //         if (arr[i] < arr[j]) {
-    //             temp[k] = arr[i];
-    //             i++;
-    //         } else{
-    //             temp[k] = arr[j];
-    //             j++; 
-    //         }
-    //         k++;
-    //     }
-    //     // left
-    //     while (i <= mid) {
-    //         temp[k++] = arr[i++];
-    //     }
-    //     // right
-    //     while (j <= endingIndex) {
-    //         temp[k++] = arr[j++];
-    //     }
+        while (i <= mid && j <= endingIndex) {
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            } else{
+                temp[k] = arr[j];
+                j++; 
+            }
+            k++;
+        }
+        // left
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        // right
+        while (j <= endingIndex) {
+            temp[k++] = arr[j++];
+        }
 
-    //     // copy temp to original arr
-    //     for(k=0, i=startingIndex; k<temp.length; k++, i++){
-    //         arr[i] = temp[k];
-    //     }
-    // }
+        // copy temp to original arr
+        for(k=0, i=startingIndex; k<temp.length; k++, i++){
+            arr[i] = temp[k];
+        }
+    }
 
     public static void main(String[] args) {
         int arr[] = {6, 3, 9, 5, 2, 8};
