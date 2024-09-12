bf5d122e1160a0c86558c18c6e6183176b9e71ca
Abhishek <Abhishek@isFuture.com>
2024-09-12 08:23:45 +0530

    quick sort file added

--- day14_Divide_Conquer/QuickSort.java
+++ day14_Divide_Conquer/QuickSort.java
@@ -1,7 +1,25 @@
 package day14_Divide_Conquer;
+/*
+Pivot and partition
+    approach of Quick sorting
+1. Pivot
+    random, median, firstelement, lastelement
+2. partition (parts)
 
+4. to call Quick sort for my left side and call Quick sort for my right
+    
+*/
 public class QuickSort {
+
+    public static void printQuick(int quickArr[]){
+        for(int i=0; i<quickArr.length; i++){
+            System.out.print(quickArr[i] + " ");
+        }    
+        System.out.println();
+    }
+
+    
     public static void main(String[] args) {
-        
+        int quickArr[] = {6, 3, 9, 8, 2, 5};
     }
 }
