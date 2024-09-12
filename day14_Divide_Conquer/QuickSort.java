87feb6aa8b80fe75622304c2cc9c4906e44612df
Abhishek <Abhishek@isFuture.com>
2024-09-12 08:40:33 +0530

    quick sort updated

--- day14_Divide_Conquer/QuickSort.java
+++ day14_Divide_Conquer/QuickSort.java
@@ -18,8 +18,39 @@ public class QuickSort {
         System.out.println();
     }
 
-    
+    public static void quickSort(int quickArr[], int sindex, int eindex){
+        if (sindex>=eindex) {
+            return;
+        }
+        // pivot find out last element
+        int pivotIndex = partition(quickArr, sindex, eindex);
+        quickSort(quickArr, sindex, pivotIndex-1);
+        quickSort(quickArr, pivotIndex+1, eindex);
+
+    }
+    public static int partition(int quickArr[], int sindex, int eindex){
+        int pivot = quickArr[eindex];
+        int i = sindex-1; // to make place for els smaller than pivot
+
+        for(int j=sindex; j<eindex; j++){
+            if (quickArr[j] <= pivot) {
+                i++;
+                //swap
+                int temp = quickArr[j];
+                quickArr[j] = quickArr[i];
+                quickArr[i] = temp;
+            }
+        }
+        i++;
+        int temp = pivot;
+        quickArr[eindex] = quickArr[i];
+        quickArr[i] = temp;
+        return i;
+    }
+
     public static void main(String[] args) {
-        int quickArr[] = {6, 3, 9, 8, 2, 5};
+        int quickArr[] = {6, 3, 9, 8, 2, 5, -5};
+        quickSort(quickArr, 0, quickArr.length-1);
+        printQuick(quickArr);
     }
 }
