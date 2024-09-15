7788d39fa0a53f8da7d74ca3a399e7cc473e0f30
Abhishek <Abhishek@isFuture.com>
2024-09-15 09:03:45 +0530

    Binary search

--- day15_Time_Complexity/binarySearch.java
+++ day15_Time_Complexity/binarySearch.java
@@ -1,23 +1,27 @@
 package day15_Time_Complexity;
 
+import java.util.Arrays;
+
 public class binarySearch {
-    public static int search(int arr[], int target){
-        int strt = 0, end = arr.length-1;
-        while (strt <= end) {
-            int mid = strt + (end - strt) / 2;
-            if (arr[mid] == target) {
-                return mid;
-            }else if (arr[mid] < target) {
-                strt = mid + 1;
+    public static int Bsearch(int arrays[], int keyy){
+        int st = 0;
+        int lst = arrays.length-1;
+        while (st <= lst) {
+            int middlee = (lst + st) / 2;
+            if (arrays[middlee] == keyy) {
+                return middlee;
+            }else if (arrays[middlee] < keyy) {
+                st = middlee + 1;
             } else{
-                mid = strt - 1;
+                lst = middlee - 1;
             }
         }  
         return -1;
     }
     public static void main(String[] args) {
-        int arr[] = {8, 9, 5, 6, 1, 2, 7, 0, 3, 4};
-        int target = 8;
-        search(arr, target);
+        int arrays[] = {8, 9, 5, 6, 1, 2, 7, 0, 3, 4};
+        Arrays.sort(arrays);
+        int keyy = 88;
+        System.out.print(Bsearch(arrays, keyy));
     }
 }
