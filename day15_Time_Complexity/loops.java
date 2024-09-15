50eafd6455471ee18d68343352ab2cc1c090737d
Abhishek <Abhishek@isFuture.com>
2024-09-15 07:32:18 +0530

    Time complexity of simple loops is O(n);

--- day15_Time_Complexity/loops.java
+++ day15_Time_Complexity/loops.java
@@ -1,5 +1,19 @@
 package day15_Time_Complexity;
 
 public class loops {
-    
+    public static void loop(int arr[]){
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            // time complexity of single loops is ****O(n)****
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 5, 6, 3};
+        
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 }
