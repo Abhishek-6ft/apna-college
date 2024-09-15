d606c0567f7f3e7a2e362797b3cb9789adf407ba
Abhishek <Abhishek@isFuture.com>
2024-09-15 08:35:53 +0530

    add optimized bubble sort

--- day15_Time_Complexity/bubbleSort.java
+++ day15_Time_Complexity/bubbleSort.java
@@ -1,15 +1,35 @@
 package day15_Time_Complexity;
 
 public class bubbleSort {
-    public static void bubbleSorting(int arr[]){
-        for (int turn = 0; turn < arr.length-1; turn++) {
+    // public static void bubbleSorting(int arr[]){
+    //     for (int turn = 0; turn < arr.length-1; turn++) {
+    //         for (int j = 0; j < arr.length-1-turn; j++) {
+    //             if (arr[j] > arr[j+1]) {
+    //                 // swaping
+    //                 int temp = arr[j];
+    //                 arr[j] = arr[j+1];
+    //                 arr[j+1] = temp;
+    //             }
+    //         }
+    //     }
+    // }
+
+    // Optimized bubble sort
+    public static void optimizedBubbleSort(int arr[]){
+        for(int turn = 0; turn<arr.length-1; turn++){
+            boolean swapped = false;
             for (int j = 0; j < arr.length-1-turn; j++) {
                 if (arr[j] > arr[j+1]) {
-                    // swaping
+                    // swap
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swapped = true;
                 }
+            }
+
+            if (swapped == false) {
+                break;
             }
         }
     }
@@ -21,7 +41,8 @@ public class bubbleSort {
     }
     public static void main(String[] args) {
         int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-        bubbleSorting(arr);
+        // bubbleSorting(arr);
+        optimizedBubbleSort(arr);
         arrayPrint(arr);
     }
 }
