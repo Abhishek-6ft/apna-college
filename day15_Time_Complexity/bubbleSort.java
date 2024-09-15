cd924d767ae5f3a1bb5d09e834a16d3275d50fd9
Abhishek <Abhishek@isFuture.com>
2024-09-15 08:26:24 +0530

    Bubble sort time complexity is O(n*2)

---
+++ day15_Time_Complexity/bubbleSort.java
@@ -0,0 +1,27 @@
+package day15_Time_Complexity;
+
+public class bubbleSort {
+    public static void bubbleSorting(int arr[]){
+        for (int turn = 0; turn < arr.length-1; turn++) {
+            for (int j = 0; j < arr.length-1-turn; j++) {
+                if (arr[j] > arr[j+1]) {
+                    // swaping
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void arrayPrint(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        bubbleSorting(arr);
+        arrayPrint(arr);
+    }
+}
