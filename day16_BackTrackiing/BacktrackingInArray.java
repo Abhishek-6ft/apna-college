bc9e155f0cffbcfcee4d294a79eb02c99d4423de
Abhishek <Abhishek@isFuture.com>
2024-09-18 07:44:40 +0530

    Print array function updated

--- day16_BackTrackiing/BacktrackingInArray.java
+++ day16_BackTrackiing/BacktrackingInArray.java
@@ -1,7 +1,18 @@
 package day16_BackTrackiing;
 
 public class BacktrackingInArray {
+    public static void changeArr(int array[], int i, int value){
+
+    }
+    public static void printArray(int array[]){
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");   
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
-        
+        int array[] = new int[5];
+        changeArr(array, 0, 1);
+        printArray(array);
     }
 }
