613b3d2ed8e403619586099f856af24b52ff4365
Abhishek <Abhishek@isFuture.com>
2024-09-18 07:48:25 +0530

    Backtracking on Array example updated with the new array decrease by two value;

--- day16_BackTrackiing/BacktrackingInArray.java
+++ day16_BackTrackiing/BacktrackingInArray.java
@@ -2,6 +2,16 @@ package day16_BackTrackiing;
 
 public class BacktrackingInArray {
     public static void changeArr(int array[], int i, int value){
+        //base case
+        if (i == array.length) {
+            printArray(array);
+            return;
+        }
+
+        //recursion
+        array[i] = value;
+        changeArr(array, i+1, value+1);
+        array[i] = array[i] - 2;
 
     }
     public static void printArray(int array[]){
