65d0d3663de031fed55d88fdf02b244887f524ed
Abhishek <Abhishek@isFuture.com>
2024-09-19 08:15:56 +0530

    find permutation example added

---
+++ day16_BackTrackiing/findPermutation.java
@@ -0,0 +1,22 @@
+package day16_BackTrackiing;
+
+public class findPermutation {
+    public static void fndPermutation(String strr, String ans){
+        // base case
+        if (strr.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        // recursion
+        for (int i = 0; i < strr.length(); i++) {
+            char current = strr.charAt(i);
+            String Newstrr = strr.substring(0, i) + strr.substring(i+1);
+            fndPermutation(Newstrr, ans+current);
+        }
+    }
+    public static void main(String[] args) {
+        String Strr = "abc";
+        fndPermutation(Strr, "");
+    }
+}
