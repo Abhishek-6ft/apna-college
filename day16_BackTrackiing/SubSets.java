1cd40c101d0c4ee596954caec9b2237fb1c8c720
Abhishek <Abhishek@isFuture.com>
2024-09-19 07:43:30 +0530

    subset example in backtracking complete

--- day16_BackTrackiing/SubSets.java
+++ day16_BackTrackiing/SubSets.java
@@ -1,13 +1,24 @@
 package day16_BackTrackiing;
 
 public class SubSets {
-    public static void FindSubSets(String str, String answer, int i){
+    public static void FindSubSets(String stri, String answer, int i){
         // base case
-
+        if (i == stri.length()) {
+            if (answer.length() == 0) {
+                System.out.println("null");
+            }else{
+                System.out.println(answer);
+            }
+            return;
+        }
         // recursion
         // yes choice
+        FindSubSets(stri, answer+stri.charAt(i), i+1);
+        // no choice
+        FindSubSets(stri, answer, i+1);
     }
     public static void main(String[] args) {
         String stri = "abc";
+        FindSubSets(stri, "", 0);
     }
 }
