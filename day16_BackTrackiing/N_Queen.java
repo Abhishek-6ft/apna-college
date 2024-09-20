ea5c0aaa6f980c24bed14693f15dfb24dad14ee8
Abhishek <Abhishek@isFuture.com>
2024-09-20 07:11:36 +0530

    Example under process

--- day16_BackTrackiing/N_Queen.java
+++ day16_BackTrackiing/N_Queen.java
@@ -1,8 +1,37 @@
 package day16_BackTrackiing;
 
 public class N_Queen {
-
+    public static void nQueen(char board[][], int row){
+        // base
+        if (row == board.length) {
+            printBoard(board);
+            return;
+        }
+        // column loop
+        for (int j = 0; j < board.length; j++) {
+            board[row][j] = 'Q';
+            nQueen(board, row+1); // function call
+            // backtracking
+            board[row][j] = '.';
+        }
+    }
+    public static void printBoard(char board[][]){
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-            
+        int n = 2;
+        char board[][] = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+
+        nQueen(board, 0);
     }
 }
