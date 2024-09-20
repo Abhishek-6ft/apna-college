88e455883e6779075336c7e8dccd79a503bf34ee
Abhishek <Abhishek@isFuture.com>
2024-09-20 07:31:53 +0530

    Safe condition updated

--- day16_BackTrackiing/N_Queen.java
+++ day16_BackTrackiing/N_Queen.java
@@ -1,6 +1,28 @@
 package day16_BackTrackiing;
 
 public class N_Queen {
+
+    public static boolean isSafe(char board[][], int row, int col){
+        // vertically up
+        for (int i = row-1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // diagnol left up
+        for (int i = row-1, j=col-1; i>=0 && j>=0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // diagnot right up
+        for (int i=row-1, j=col+1; i>=0 && j<board.length; i--,j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
     public static void nQueen(char board[][], int row){
         // base
         if (row == board.length) {
@@ -9,13 +31,16 @@ public class N_Queen {
         }
         // column loop
         for (int j = 0; j < board.length; j++) {
-            board[row][j] = 'Q';
-            nQueen(board, row+1); // function call
-            // backtracking
-            board[row][j] = '.';
+            if(isSafe(board, row, j)){
+                board[row][j] = 'Q';
+                nQueen(board, row+1); // function call
+                // backtracking
+                board[row][j] = '_';
+            }
         }
     }
     public static void printBoard(char board[][]){
+        System.out.println("------------chess board--------------");
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 System.out.print(board[i][j]);
@@ -28,7 +53,7 @@ public class N_Queen {
         char board[][] = new char[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                board[i][j] = '.';
+                board[i][j] = '_';
             }
         }
 
