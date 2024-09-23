b2e9e7c7824e2166ade2dd9ab791693690db3525
Abhishek <Abhishek@isFuture.com>
2024-09-23 08:03:55 +0530

    Size Operation in ArrayLists was updated

---
+++ day17_ArrayList/ArrayListSizeOperation.java
@@ -0,0 +1,21 @@
+package day17_ArrayList;
+
+import java.util.ArrayList;
+public class ArrayListSizeOperation {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        System.out.println(list.size());
+
+        // print the arraylists
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
