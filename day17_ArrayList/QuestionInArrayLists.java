229d59b2272d1c6e182b3a264cef57bc59b75df9
Abhishek <Abhishek@isFuture.com>
2024-09-23 08:15:06 +0530

    Maximum in Array list updat

---
+++ day17_ArrayList/QuestionInArrayLists.java
@@ -0,0 +1,24 @@
+package day17_ArrayList;
+
+import java.util.ArrayList;
+public class QuestionInArrayLists {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+        
+        // maximum in arraylist
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < list.size(); i++) {
+            // if (max < list.get(i)) {
+            //     max = list.get(i);
+            // }
+            max = Math.max(max, list.get(i));
+        }
+
+        System.out.println("Max element = " + max);
+    }
+}
