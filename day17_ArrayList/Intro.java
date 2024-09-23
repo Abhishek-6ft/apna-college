ea1246338ed543deab61cdd439107b61218f2b8b
Abhishek <Abhishek@isFuture.com>
2024-09-23 07:41:26 +0530

    Syntax was added

--- day17_ArrayList/Intro.java
+++ day17_ArrayList/Intro.java
@@ -1,5 +1,14 @@
 package day17_ArrayList;
 
+
+import java.util.ArrayList;
+
 public class Intro {
-    
+    public static void main(String[] args) {
+        // Java Collection Framework
+        // Syntax For Array Lists
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Boolean> list2 = new ArrayList<>();
+        ArrayList<String> list3 = new ArrayList<>();
+    }
 }
