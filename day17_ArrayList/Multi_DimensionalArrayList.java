e5ba6ec93c3ca787daf1eacbe5ae7bf85b9f49ef
Abhishek <Abhishek@isFuture.com>
2024-09-24 08:13:50 +0530

    Dimensional Array print in Array list

--- day17_ArrayList/Multi_DimensionalArrayList.java
+++ day17_ArrayList/Multi_DimensionalArrayList.java
@@ -4,20 +4,31 @@ import java.util.*;
 public class Multi_DimensionalArrayList {
     public static void main(String[] args) {
         ArrayList<ArrayList<Integer>> mainList = new ArrayList<>();
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1); list.add(2); list.add(3);
-        mainList.add(list);
-
+        ArrayList<Integer> list1 = new ArrayList<>();
         ArrayList<Integer> list2 = new ArrayList<>();
-        list2.add(4); list2.add(5); list2.add(6);
-        mainList.add(list2);
+        ArrayList<Integer> list3 = new ArrayList<>();
 
-        for(int i=0; i<mainList.size(); i++){
-            ArrayList<Integer> currentList = mainList.get(i);
-            for(int j=0; j<currentList.size(); j++){
-                System.out.print(currentList.get(j) + " ");
-            }
+        for (int i = 1; i <= 5; i++) {
+            list1.add(i*1); // 1 2 3 4 5
+            list2.add(i*2); // 2 4 6 8 10
+            list3.add(i*3); // 3 6 9 12 15
+
         }
+
+        mainList.add(list1);
+        mainList.add(list2);
+        mainList.add(list3);
+
         System.out.println(mainList);
+
+        // nested loop for print 
+        for(int i = 0; i < mainList.size(); i++){
+            ArrayList<Integer> currentlist = mainList.get(i);
+            for (int j = 0; j < currentlist.size(); j++) {
+                System.out.print(currentlist.get(j) + " ");
+            }
+            System.out.println();
+        }
+        // System.out.println(mainList);
     }
 }
