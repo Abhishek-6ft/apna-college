9bc0d01c0c8d97733d42195c9073b7c957026e7b
Abhishek <Abhishek@isFuture.com>
2024-09-26 09:01:59 +0530

    array updated

--- day17_ArrayList/ContainerWithMostWater_B.java
+++ day17_ArrayList/ContainerWithMostWater_B.java
@@ -23,7 +23,7 @@ public class ContainerWithMostWater_B {
     public static void main(String[] args) {
         ArrayList<Integer> height = new ArrayList<>();
         // 1 8 6 2 5 4 8 3 7
-        height.add(1);
+        height.add(100);
         height.add(8);
         height.add(6);
         height.add(2);
@@ -31,7 +31,7 @@ public class ContainerWithMostWater_B {
         height.add(4);
         height.add(8);
         height.add(3);
-        height.add(7);
+        height.add(70);
         System.out.println(storeWater(height));
     }
 }
