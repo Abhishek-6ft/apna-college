65ddda2d2e534ee75dcf51eda507145c14535e7a
Abhishek <Abhishek@isFuture.com>
2024-10-01 07:55:54 +0530

    "updated"

--- day17_ArrayList/PairSum.java
+++ day17_ArrayList/PairSum.java
@@ -3,4 +3,29 @@ import java.util.*;
 
 public class PairSum {
     
+    // brute force
+    public static boolean pairsum1(ArrayList<Integer> list, int target){
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i+1; j < list.size(); j++) {
+                if (list.get(i) + list.get(j) == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        int target = 50;
+        System.out.println(pairsum1(list, target));
+
+    }
 }
