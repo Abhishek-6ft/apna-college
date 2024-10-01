237151cb78df51676cf80e90104d8547a600ae76
Abhishek <Abhishek@isFuture.com>
2024-10-01 07:47:05 +0530

    "file updated"

--- day17_ArrayList/PointerApproach.java
+++ day17_ArrayList/PointerApproach.java
@@ -1,8 +1,31 @@
 package day17_ArrayList;
 
 import java.util.*;
+
 public class PointerApproach {
-    
+
+    public static int mostStoreWater(ArrayList<Integer> height) {
+        int maximumWater = 0;
+        int leftPointer = 0;
+        int rightPointer = height.size() - 1;
+        while (leftPointer < rightPointer) {
+            // calculate water area
+            int heightofTank = Math.min(height.get(leftPointer), height.get(rightPointer));
+            int widthOfTank = rightPointer - leftPointer;
+            int currentWater = heightofTank * widthOfTank;
+            maximumWater = Math.max(maximumWater, currentWater);
+
+            // update pointer
+            if (height.get(leftPointer) < height.get(rightPointer)) {
+                leftPointer++;
+            } else {
+                rightPointer--;
+            }
+
+        }
+        return maximumWater;
+    }
+
     public static void main(String[] args) {
         ArrayList<Integer> height = new ArrayList<>();
         // 1 8 6 2 5 4 8 3 7
@@ -16,5 +39,6 @@ public class PointerApproach {
         height.add(3);
         height.add(7);
         System.out.println(height);
+        System.out.println(mostStoreWater(height));
     }
 }
