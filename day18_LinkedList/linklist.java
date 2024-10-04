c01c4f7a2c8eeef3c8c5b23c407191fc62734868
Abhishek <Abhishek@isFuture.com>
2024-10-04 08:03:01 +0530

    linked list file added

---
+++ day18_LinkedList/linklist.java
@@ -0,0 +1,17 @@
+package day18_LinkedList;
+
+public class linklist {
+
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data  = data;
+            this.next = null;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
