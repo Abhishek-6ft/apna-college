074976b3ac710e0084fa24be06cd93539bc98449
Abhishek <Abhishek@isFuture.com>
2024-10-25 08:52:17 +0530

    Stack using Linked list updated

---
+++ day19_Stack/StackUsingLinkedlist.java
@@ -0,0 +1,63 @@
+package day19_Stack;
+import java.util.LinkedList;
+import java.util.Stack;
+public class StackUsingLinkedlist {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    static class Stack{
+        static Node head = null;
+
+        //isempty
+        public static boolean isEmpty(){
+            return head == null;
+        }
+
+        // push
+        public static void push(int data){
+            Node newNode = new Node(data);
+            if (isEmpty()) {
+                head = newNode;
+                return;
+            }
+
+            newNode.next = head;
+            head = newNode;
+        }
+
+        // pop
+
+        public static int pop(){
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        //peek 
+        public static int peek(){
+            if (isEmpty()) {
+                return -1;
+            }
+            return head.data;
+        }
+    }
+    public static void main(String[] args) {
+        Stack st = new Stack();
+        st.push(0);
+        st.push(0);
+        st.push(0);
+
+        while (!st.isEmpty()) {
+            System.out.println(st.peek());
+            st.pop();
+        }
+    }
+}
