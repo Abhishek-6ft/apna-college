47e141c09804b18f6d2db25a9e0a0245317394e3
Abhishek <Abhishek@isFuture.com>
2024-11-29 08:37:41 +0530

    Level order function also added in this folder

--- day22_BinaryTree/BuildTreePreOrder.java
+++ day22_BinaryTree/BuildTreePreOrder.java
@@ -1,22 +1,24 @@
 package day22_BinaryTree;
+
 import java.util.*;
 
 public class BuildTreePreOrder {
-    static class Node{
+    static class Node {
         int data;
         Node left;
         Node right;
 
-        Node(int data){
+        Node(int data) {
             this.data = data;
             this.left = null;
             this.right = null;
         }
     }
 
-    public static class BinaryTree{
+    public static class BinaryTree {
         static int indx = -1;
-        public static Node buildTree(int nodes[]){
+
+        public static Node buildTree(int nodes[]) {
             indx++;
             if (nodes[indx] == -1) {
                 return null;
@@ -28,9 +30,10 @@ public class BuildTreePreOrder {
 
             return newNode;
         }
-        
-        public static void preOrder(Node root){
-            if(root == null){
+
+        // Pre order traversal
+        public static void preOrder(Node root) {
+            if (root == null) {
                 return;
             }
             System.out.print(root.data + " ");
@@ -38,7 +41,8 @@ public class BuildTreePreOrder {
             preOrder(root.right);
         }
 
-        public static void inOrder(Node root){
+        // In order traversal
+        public static void inOrder(Node root) {
             if (root == null) {
                 return;
             }
@@ -47,7 +51,8 @@ public class BuildTreePreOrder {
             inOrder(root.right);
         }
 
-        public static void postOrder(Node root){
+        // post order traversal
+        public static void postOrder(Node root) {
             if (root == null) {
                 return;
             }
@@ -55,17 +60,49 @@ public class BuildTreePreOrder {
             postOrder(root.right);
             System.out.print(root.data + " ");
         }
+
+        // Level order traversal
+        public static void levelOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while (!q.isEmpty()) {
+                Node currNode = q.remove();
+                if (currNode == null) {
+                    System.out.println();
+                    if (q.isEmpty()) {
+                        break;
+                    } else {
+                        q.add(null);
+                    }
+                } else {
+                    System.err.print(currNode.data + " ");
+                    if (currNode.left != null) {
+                        q.add(currNode.left);
+                    }
+                    if (currNode.right != null) {
+                        q.add(currNode.right);
+                    }
+                }
+            }
+        }
     }
 
-
     public static void main(String[] args) {
-        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTree tree = new BinaryTree();
-        Node root = tree.buildTree(nodes); 
-        tree.preOrder(root);  
-        System.out.println(); 
-        tree.inOrder(root);   
+        Node root = tree.buildTree(nodes);
+        tree.preOrder(root);
+        System.out.println();
+        tree.inOrder(root);
         System.out.println();
         tree.postOrder(root);
-    }    
+        System.out.println();
+        tree.levelOrder(root);
+    }
 }
