8f2c788b8dd7f801e61a0f90f2910d67706715a6
Abhishek <Abhishek@isFuture.com>
2024-11-30 08:31:38 +0530

    diameter of tree problem solve with approach of O(n)

--- day22_BinaryTree/HeightOfTree.java
+++ day22_BinaryTree/HeightOfTree.java
@@ -48,14 +48,14 @@ public class HeightOfTree {
     }
 
     // Diameter of tree
-    public static int diameter(Node root) { // O{n^2}
+    public static int diameter2(Node root) { // O{n^2}
         if (root == null) {
             return 0;
         }
 
-        int leftDiameter = diameter(root.left);
+        int leftDiameter = diameter2(root.left);
         int leftHeight = height(root.left);
-        int rightDiameter = diameter(root.right);
+        int rightDiameter = diameter2(root.right);
         int rightHeight = height(root.right);
 
         int selfDiameter = leftHeight + rightHeight + 1;
@@ -64,11 +64,35 @@ public class HeightOfTree {
 
     }
 
+    // Diameter of tree with the O(n) time complexity approach
+
+    static class Info {
+        int diam;
+        int ht;
+
+        public Info(int diam, int ht){
+            this.diam = diam;
+            this.ht = ht;
+        }
+    }
+    public static Info diameter(Node root) {
+        if (root == null) {
+            return new Info(0, 0);
+        }
+        Info leftInfo = diameter(root.left);
+        Info rightInfo = diameter(root.right);
+
+        int diam = Math.max(Math.max(leftInfo.diam, rightInfo.diam) , leftInfo.ht + rightInfo.ht + 1);
+        int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1;
+
+        return new Info(diam, ht);
+    }
+
     public static void main(String[] args) {
         /*
-         * 1
-         * / \
-         * 2 3
+         *    1
+         *   / \
+         *  2   3
          * / \ / \
          * 4 5 6 7
          */
@@ -84,7 +108,9 @@ public class HeightOfTree {
         System.out.println(height(root));
         System.out.println(count(root));
         System.out.println(sum(root));
-        System.out.println("diameter of root = "+diameter(root));
+        System.out.println("diameter of root = "+diameter2(root));
+        System.out.println(diameter(root).diam);
+
 
     }
 }
