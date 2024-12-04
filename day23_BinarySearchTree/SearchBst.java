645bc314fc5b2c1945afe29f94cc89ecba7dc637
Abhishek <Abhishek@isFuture.com>
2024-12-04 08:36:26 +0530

    Search in Binary Search Tree function done

---
+++ day23_BinarySearchTree/SearchBst.java
@@ -0,0 +1,66 @@
+package day23_BinarySearchTree;
+
+import day04_Loops.forloops;
+
+public class SearchBst {
+    static class Node{
+        int data;
+        Node left, right;
+        public Node(int data){
+            this.data = data;
+        }
+    }
+    public static Node insert(Node root, int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+        if (root.data > val) {
+            //left subtree
+            root.left = insert(root.left, val);
+        } else {
+            //right subtree
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+    public static void inOrder(Node root){
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data+ " ");
+        inOrder(root.right);
+    }
+    public static boolean search(Node root, int key){
+        if (root == null) {
+            return false;
+        }
+        if (root.data == key) {
+            return true;
+        }
+
+        if (root.data > key) {
+            return search(root.left, key);
+        } else {
+            return search(root.right, key);
+        }
+
+    }
+    public static void main(String[] args) {
+        int values[] = {1, 3, 5, 4, 2, 7, 6, 10, 9, 8};
+        Node root = null;
+
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+        inOrder(root);
+        System.out.println();
+
+        if (search(root, 2)) {
+            System.out.println("found");
+        } else {
+            System.out.println("not found");
+        }
+    }
+}
