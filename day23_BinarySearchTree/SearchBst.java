6c46a7d8ca91117b3d3c1346936be71accafcb32
Abhishek <Abhishek@isFuture.com>
2024-12-05 07:46:12 +0530

    Print in Range function updated in this file

--- day23_BinarySearchTree/SearchBst.java
+++ day23_BinarySearchTree/SearchBst.java
@@ -1,39 +1,43 @@
 package day23_BinarySearchTree;
 
 public class SearchBst {
-    static class Node{
+    static class Node {
         int data;
         Node left, right;
-        public Node(int data){
+
+        public Node(int data) {
             this.data = data;
         }
     }
+
     // Insert function of BST
-    public static Node insert(Node root, int val){
-        if(root == null){
+    public static Node insert(Node root, int val) {
+        if (root == null) {
             root = new Node(val);
             return root;
         }
         if (root.data > val) {
-            //left subtree
+            // left subtree
             root.left = insert(root.left, val);
         } else {
-            //right subtree
+            // right subtree
             root.right = insert(root.right, val);
         }
         return root;
     }
+
     // Inorder in BST
-    public static void inOrder(Node root){
+    public static void inOrder(Node root) {
         if (root == null) {
             return;
         }
         inOrder(root.left);
-        System.out.print(root.data+ " ");
+        System.out.print(root.data + " ");
         inOrder(root.right);
     }
+
     // Search Function in BST
-    public static boolean search(Node root, int key){
+    public static boolean search(Node root, int key) {
         if (root == null) {
             return false;
         }
@@ -49,19 +53,19 @@ public class SearchBst {
 
     }
 
-    public static Node delete(Node root, int val){
+    // This function help to delete Node from a Node
+    public static Node delete(Node root, int val) {
         if (root.data < val) {
             root.right = delete(root.right, val);
         } else if (root.data > val) {
             root.left = delete(root.left, val);
-        }
-        else {// voilla
-            //case 1 leaf node
+        } else {// voilla
+                // case 1 leaf node
             if (root.left == null && root.right == null) {
                 return null;
             }
-            
-            //case 2 - single child
+
+            // case 2 - single child
             if (root.left == null) {
                 return root.right;
             } else if (root.right == null) {
@@ -72,34 +76,55 @@ public class SearchBst {
             Node Is = findInorderSuccessor(root.right);
             root.data = Is.data;
             root.right = delete(root.right, Is.data);
-            
+
         }
         return root;
     }
-    public static Node findInorderSuccessor(Node root){
+
+    public static Node findInorderSuccessor(Node root) {
         while (root.left != null) {
             root = root.left;
         }
         return root;
     }
 
+    // THis function print IN Range of a Node like print in k1 to k2
+    public static void printInRange(Node root, int k1, int k2) {
+        if (root == null) {
+            return;
+        }
+        if (root.data >= k1 && root.data <= k2) {
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data +" ");
+            printInRange(root.right, k1, k2);
+        } 
+        else if (root.data < k1) {
+            printInRange(root.left, k1, k2);
+        } 
+        else {
+            printInRange(root.right, k1, k2);
+        }
+    }
+
     public static void main(String[] args) {
-        int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
+        int values[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
         Node root = null;
 
         for (int i = 0; i < values.length; i++) {
             root = insert(root, values[i]);
         }
         inOrder(root);
+        System.out.println();
+        printInRange(root, 5, 12);
+        // inOrder(root);
         // System.out.println();
 
-        delete(root, 11);
-        delete(root, 10);
-        delete(root, 5);
+        // delete(root, 11);
+        // delete(root, 10);
+        // delete(root, 5);
 
+        // System.out.println();
+        // inOrder(root);
 
-        System.out.println();
-        inOrder(root);
-        
     }
 }
