0451c20a41fe5a17f3aad1c7176effe62a07ec47
Abhishek <Abhishek@isFuture.com>
2024-12-09 07:43:47 +0530

    Priority Queue file updated how to use Priority Queue using java collection framework

--- day25_PriorityQueue_Heaps/PriorityQueueInJcF.java
+++ day25_PriorityQueue_Heaps/PriorityQueueInJcF.java
@@ -1,16 +1,34 @@
 package day25_PriorityQueue_Heaps;
-import java.util.PriorityQueue;
+import java.util.*;
+
+
 public class PriorityQueueInJcF {
-    
+
+    static class Student implements Comparable<Student> {
+        String name;
+        int rank;
+
+        public Student(String name, int rank){
+            this.name = name;
+            this.rank = rank;
+        }
+        @Override
+        public int compareTo(Student s2) {
+            return this.rank - s2.rank;
+        }
+        
+    }
+
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        pq.add(3);
-        pq.add(4);
-        pq.add(1);
-        pq.add(7);
+        PriorityQueue<Student> pq = new PriorityQueue<>();
+
+        pq.add(new Student("Abhishek", 15));
+        pq.add(new Student("Raju", 21));
+        pq.add(new Student("Parbhat", 20));
+        pq.add(new Student("Priyanshu", 19));
 
         while (!pq.isEmpty()) {
-            System.out.println(pq.peek());
+            System.out.println(pq.peek().name + " -> " + pq.peek().rank);
             pq.remove();
         }
     }
