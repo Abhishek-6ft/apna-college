6d95f8ee7fe0d64465d02d4616981682fafaa2d8
Abhishek <Abhishek@isFuture.com>
2025-01-03 09:16:12 +0530

    Find cost for connecting n ropes and cost will be minimum

--- day25_PriorityQueue_Heaps/NearByCars.java
+++ day25_PriorityQueue_Heaps/NearByCars.java
@@ -1,5 +1,7 @@
 package day25_PriorityQueue_Heaps;
+
 import java.util.PriorityQueue;
+
 public class NearByCars {
     static class Point implements Comparable<Point> {
         int x;
@@ -7,7 +9,7 @@ public class NearByCars {
         int distsq;
         int indx;
 
-        public Point(int x, int y, int distsq, int indx){
+        public Point(int x, int y, int distsq, int indx) {
             this.x = x;
             this.y = y;
             this.distsq = distsq;
@@ -15,23 +17,23 @@ public class NearByCars {
         }
 
         @Override
-        public int compareTo(Point p2){
+        public int compareTo(Point p2) {
             return this.distsq - p2.distsq; // sorting in ascending order
         }
     }
 
     public static void main(String[] args) {
-        int pointers[][] = {{3, 3}, {5, -1}, {-2, 4}};
+        int pointers[][] = { { 3, 3 }, { 5, -1 }, { -2, 4 } };
         int k = 2;
 
         PriorityQueue<Point> pq = new PriorityQueue<>();
-        for(int i = 0; i < pointers.length; i++){
-            int distsq = pointers[i][0]*pointers[i][0] + pointers[i][1]*pointers[i][1];
+        for (int i = 0; i < pointers.length; i++) {
+            int distsq = pointers[i][0] * pointers[i][0] + pointers[i][1] * pointers[i][1];
             pq.add(new Point(pointers[i][0], pointers[i][1], distsq, i));
         }
 
-        //near car
-        for(int i=0; i<k; i++){
+        // near car
+        for (int i = 0; i < k; i++) {
             System.out.println("C" + pq.remove().indx);
         }
     }
